14dc8a66051c73403fcab52b9dfc412c467002a3
Yangshun Tay <tay.yang.shun@gmail.com>
2018-05-05 23:29:43 -0700

    Add CSS minification step

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 'use strict';
 
 const gulp = require('gulp');
+const cssnano = require('gulp-cssnano');
+const rename = require('gulp-rename');
 const sass = require('gulp-sass');
 const webserver = require('gulp-webserver');
 
@@ -8,6 +10,9 @@ gulp.task('sass', function() {
   return gulp
     .src('./scss/themes/**/*.scss')
     .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./css'))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(cssnano())
     .pipe(gulp.dest('./css'));
 });
 
