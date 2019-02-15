24508a8ec8b6ae8684148bdceb351b9804bff222
Yangshun Tay <tay.yang.shun@gmail.com>
2019-02-15 12:39:58 +0800

    Abstract out radio behavior

--- js/pills.js
+++ js/pills.js
@@ -1,12 +1,1 @@
-function pills($elements) {
-  $elements.forEach($pills => {
-    $pills.addEventListener('click', (e) => {
-      if (e.target && e.target.classList.contains('pill-item')) {
-        $pills.querySelectorAll('.pill-item').forEach($pillItem => 
-          $pillItem.classList.remove('pill-active')
-        );
-        e.target.classList.add('pill-active');
-      }
-    });
-  });
-}
+const pills = makeRadioBehavior('click', 'pill-item', 'pill-active');
--- js/tabs.js
+++ js/tabs.js
@@ -1,12 +1,1 @@
-function tabs($elements) {
-  $elements.forEach($tabs => {
-    $tabs.addEventListener('click', (e) => {
-      if (e.target && e.target.classList.contains('tab-item')) {
-        $tabs.querySelectorAll('.tab-item').forEach($tabItem => 
-          $tabItem.classList.remove('tab-active')
-        );
-        e.target.classList.add('tab-active');
-      }
-    });
-  });
-}
+const tabs = makeRadioBehavior('click', 'tab-item', 'tab-active');
