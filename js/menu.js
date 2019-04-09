f80bb69f2f9821c5e2383bc1ffa7ea8a6c4e9682
Yangshun Tay <tay.yang.shun@gmail.com>
2019-04-08 20:27:23 -0700

    Remove redundant menu classes

--- js/menu.js
+++ js/menu.js
@@ -1,14 +1,14 @@
 function menu($elements) {
   $elements.forEach($menu => {
     $menu.addEventListener('click', e => {
-      if (e.target && e.target.classList.contains('menu-item')) {
+      if (e.target && e.target.classList.contains('menu-link')) {
         if (e.target.parentNode.classList.contains('menu-list-item')) {
           e.target.parentNode.classList.toggle('menu-list-item-collapsed');
         }
       }
 
       $menu
-        .querySelectorAll('.menu-item')
+        .querySelectorAll('.menu-link')
         .forEach($elItem => $elItem.classList.remove('menu-link-active'));
       event.target.classList.add('menu-link-active');
 
