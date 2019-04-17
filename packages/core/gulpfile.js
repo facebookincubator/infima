0e3cc880322b4ecc08b2054533f42e2fe942a5f5
Yangshun Tay <tay.yang.shun@gmail.com>
2019-04-16 22:59:46 -0700

    Add postcss pipeline for autoprefixing, cssnano and css variables

--- packages/core/gulpfile.js
+++ packages/core/gulpfile.js
@@ -1,43 +1,66 @@
 'use strict';
 
+const autoprefixer = require('autoprefixer');
+const cssVariables = require('postcss-css-variables');
+const cssnano = require('cssnano');
+const del = require('del');
 const gulp = require('gulp');
-const cssnano = require('gulp-cssnano');
 const rename = require('gulp-rename');
+const postcss = require('gulp-postcss');
 const sass = require('gulp-sass');
 const webserver = require('gulp-webserver');
 
 function sassTask() {
+  return (
+    gulp
+      .src('./scss/themes/**/*.scss')
+      .pipe(sass().on('error', sass.logError))
+      .pipe(postcss([autoprefixer()]))
+      .pipe(gulp.dest('./dist'))
+      // Compile CSS variables to its values for IE11 support.
+      .pipe(rename({ suffix: '.compat' }))
+      .pipe(postcss([cssVariables()]))
+      .pipe(gulp.dest('./dist'))
+  );
+}
+
+function minify() {
   return gulp
-    .src('./scss/themes/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./dist'))
+    .src('./dist/**/*.css')
     .pipe(rename({ suffix: '.min' }))
-    .pipe(cssnano())
+    .pipe(postcss([cssnano()]))
     .pipe(gulp.dest('./dist'));
 }
 
-function copyStylesTask() {
+function copyStyles() {
   return gulp.src('./dist/**/*.css').pipe(gulp.dest('./demo/css'));
 }
 
-function copyScriptsTask() {
+function copyScripts() {
   return gulp.src('./js/**/*.js').pipe(gulp.dest('./demo/js'));
 }
 
-const copyFilesTask = gulp.parallel(copyStylesTask, copyScriptsTask);
-
-function serveTask(cb) {
+function serve() {
   return gulp.src('./demo').pipe(
     webserver({
-      livereload: true,
+      livereload: true, // Not working. Figure out why.
       open: true,
     }),
   );
 }
 
-const compileAndCopy = gulp.series(sassTask, copyFilesTask);
+function clean() {
+  return del(['./dist/**/*', './demo/css/**', './demo/js/**']);
+}
 
-gulp.watch(['./scss/**/*.scss'], {}, compileAndCopy);
+const copyFiles = gulp.parallel(copyStyles, copyScripts);
+const compileAndCopy = gulp.series(sassTask, copyFiles);
 
-exports.build = compileAndCopy;
-exports.default = gulp.series(compileAndCopy, serveTask);
+function watch(cb) {
+  gulp.watch(['./scss/**/*.scss'], compileAndCopy);
+  cb();
+}
+
+exports.clean = clean;
+exports.build = gulp.series(clean, gulp.series(sassTask, minify, copyFiles));
+exports.default = gulp.series(clean, watch, compileAndCopy, serve);
