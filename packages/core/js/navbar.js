3f060fe804e12f8f5f9b2d47c4e5d7864899a23f
Yangshun Tay <tay.yang.shun@gmail.com>
2019-06-09 13:37:51 -0700

    Tweak navbar classes

--- packages/core/js/navbar.js
+++ packages/core/js/navbar.js
@@ -2,21 +2,21 @@ function navbar($elements) {
   $elements.forEach($navbar => {
     // TODO: Use data-toggle approach.
     const $toggle = $navbar.querySelector('.navbar__toggle');
-    const $sidebar = $navbar.querySelector('.navbar__sidebar');
-    const $sidebarClose = $navbar.querySelector('.navbar__sidebar__close');
-    const $backdrop = $navbar.querySelector('.navbar__sidebar__backdrop');
+    const $sidebar = $navbar.querySelector('.navbar-sidebar');
+    const $sidebarClose = $navbar.querySelector('.navbar-sidebar__close');
+    const $backdrop = $navbar.querySelector('.navbar-sidebar__backdrop');
 
     if ($toggle == null || $sidebarClose == null) {
       return;
     }
 
     $toggle.addEventListener('click', e => {
-      $navbar.classList.add('navbar--sidebar-show');
+      $navbar.classList.add('navbar-sidebar--show');
     });
 
     [$backdrop, $sidebarClose].forEach($el =>
       $el.addEventListener('click', e => {
-        $navbar.classList.remove('navbar--sidebar-show');
+        $navbar.classList.remove('navbar-sidebar--show');
       }),
     );
   });
