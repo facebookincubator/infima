542d82728efc011d3c250ec050f4dddb1bcdc0d8
Yangshun Tay <tay.yang.shun@gmail.com>
2019-10-28 14:58:29 +0800

    Deploy website version based on 15809ae010dfb92f63e3e1dc5f54959b023f57eb

---
+++ metadata---infima-docs-templated-4-f-1fb.25fb4314c1fea8befd51.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[43],{161:function(e){e.exports=JSON.parse('{"id":"template","title":"Template","description":"import Playground from \'@theme/Playground\';","source":"@site/docs/TEMPLATE.md","permalink":"/infima/docs/template"}')}}]);
