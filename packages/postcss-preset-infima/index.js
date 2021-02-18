909593def2f6e50f696a96b5a408724ee944320b
Alexey Pyltsyn <lex61rus@gmail.com>
2021-02-18 07:20:07 +0300

    refactor: various clean-ups and fixes (#69)

--- packages/postcss-preset-infima/index.js
+++ packages/postcss-preset-infima/index.js
@@ -7,23 +7,19 @@
 
 'use strict';
 
-const autoprefixer = require('autoprefixer');
 const postcssEach = require('postcss-each');
 const postcssEasyImport = require('postcss-easy-import');
 const postcssFor = require('postcss-for');
 const postcssPresetEnv = require('postcss-preset-env');
-const postcssStripInlineComments = require('postcss-strip-inline-comments');
 const postcssNested = require('postcss-nested');
 const postcssNestedAncestors = require('postcss-nested-ancestors');
 const postcssMixins = require('postcss-mixins');
 const postcssCombineDuplicatedSelectors = require('postcss-combine-duplicated-selectors');
 const postcssSortMediaQueries = require('postcss-sort-media-queries');
-const scss = require('postcss-scss');
 
 module.exports = (options) => ({
   plugins: [
     postcssEasyImport({ prefix: '_' }),
-    postcssStripInlineComments,
     postcssEach,
     postcssFor,
     postcssNestedAncestors,
@@ -58,5 +54,4 @@ module.exports = (options) => ({
     postcssSortMediaQueries,
     postcssCombineDuplicatedSelectors,
   ].filter(Boolean),
-  syntax: scss,
 });
