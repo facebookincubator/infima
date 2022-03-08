0d4d46fb6b2e616909bfaa88e5946ccadedc37e1
Reece Dunham <me@rdil.rocks>
2022-03-08 15:21:45 -0500

    Update dependencies

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -20,7 +20,8 @@ function Home() {
   return (
     <Layout
       title={siteConfig.title}
-      description="A modern styling framework for content-driven websites">
+      description="A modern styling framework for content-driven websites"
+    >
       <header className={classnames('hero', styles.heroBanner)}>
         <div className="container">
           <img
@@ -35,7 +36,8 @@ function Home() {
                 'button button--primary button--lg',
                 styles.getStarted,
               )}
-              to={useBaseUrl('docs/getting-started/introduction')}>
+              to={useBaseUrl('docs/getting-started/introduction')}
+            >
               Get Started&nbsp;&nbsp;→
             </Link>
           </div>
@@ -54,7 +56,8 @@ function Home() {
                       className={styles.sectionDarkLink}
                       href="https://docusaurus.io"
                       rel="noreferrer noopener"
-                      target="_blank">
+                      target="_blank"
+                    >
                       Docusaurus 2
                     </a>
                   </h2>
